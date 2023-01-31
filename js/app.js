a91104db9a6ffe6369c9a3691193748658c4a538
Kyle Freemantle <kcfreemantle@gmail.com>
2023-01-30 18:36:46 -0800

    actually removed the comment from the test array for question 5, tested OK

--- js/app.js
+++ js/app.js
@@ -115,7 +115,7 @@ function multiplyArray(multArr) { //eslint-disable-line
   }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
